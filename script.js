bd5dcfc699b1a2f033f76d0bca63343283720fe7
Josh Trivedi <macosh@Joshs-MacBook-Pro.local>
2023-06-20 06:32:43 +0200

    feat: search functionality

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
-var _a, _b, _c, _d;
+var _a, _b, _c, _d, _e;
 // Fetch button click event listener
 (_a = document.getElementById("fetchButton")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => {
     var _a;
@@ -15,8 +15,6 @@ var _a, _b, _c, _d;
 });
 // Process the selected email files
 function processEmailFiles(files) {
-    const data = [];
-    // Read each file using FileReader
     // Read each file using FileReader
     for (const file of files) {
         const reader = new FileReader();
@@ -24,25 +22,51 @@ function processEmailFiles(files) {
             var _a;
             const text = (_a = event.target) === null || _a === void 0 ? void 0 : _a.result;
             const emails = text.split(",").map((email) => email.trim());
-            displayTableData(emails);
+            displayTableData(emails, files, file.name);
         };
         reader.readAsText(file);
     }
 }
 // Display table data
-function displayTableData(emails) {
+function displayTableData(emails, files, fileName) {
     const tableBody = document.getElementById('tableBody');
     if (tableBody) {
         tableBody.innerHTML = '';
         emails.forEach((email, index) => {
             const newRow = document.createElement('tr');
             newRow.innerHTML = `
-          <td class="px-4 py-2">${index + 1}</td>
-          <td class="px-4 py-2">${email}</td>
-        `;
+        <td class="px-4 py-2">${index + 1}</td>
+        <td class="px-4 py-2">${email}</td>
+        <td class="px-4 py-2">${fileName}</td>
+
+      `;
+            newRow.addEventListener('click', () => {
+                selectRow(newRow, index, email, emails, fileName, files);
+            });
             tableBody.appendChild(newRow);
         });
     }
+}
+// Select row and enable editing
+function selectRow(row, index, email, emails, fileName, files) {
+    const selectedRow = document.querySelector('.selected');
+    if (selectedRow) {
+        selectedRow.classList.remove('selected');
+    }
+    row.classList.add('selected');
+    const inputValue = prompt('Edit email value:', email);
+    if (inputValue !== null) {
+        // Update the email value in the table
+        row.children[1].textContent = inputValue;
+        // Update the email value in the data array
+        emails[index] = inputValue;
+        // Update the text file
+        updateTextFile(emails, fileName, files);
+    }
+}
+// Update the text file with new email values
+function updateTextFile(emails, fileName, files) {
+    const updatedText = emails.join(', ');
 }
 // Add to table button click event listener
 (_c = document.getElementById("addToTableButton")) === null || _c === void 0 ? void 0 : _c.addEventListener("click", () => {
@@ -62,12 +86,34 @@ function displayTableData(emails) {
 });
 // Delete last row button click event listener
 (_d = document
-    .getElementById("removeLastRowButton")) === null || _d === void 0 ? void 0 : _d.addEventListener("click", () => {
-    var _a;
-    const tableRows = (_a = document
-        .getElementById("tableBody")) === null || _a === void 0 ? void 0 : _a.getElementsByTagName("tr");
-    if (tableRows && tableRows.length > 0) {
-        tableRows[tableRows.length - 1].remove();
+    .getElementById("addToFile")) === null || _d === void 0 ? void 0 : _d.addEventListener("click", () => {
+});
+// Search for an email by ID or value
+function searchEmail() {
+    var _a, _b, _c;
+    const searchInput = document.getElementById("manualRowField");
+    const searchTerm = searchInput.value.trim().toLowerCase();
+    const tableRows = (_a = document.getElementById("tableBody")) === null || _a === void 0 ? void 0 : _a.getElementsByTagName("tr");
+    if (tableRows) {
+        for (const row of tableRows) {
+            const idCell = row.getElementsByTagName("td")[0];
+            const valueCell = row.getElementsByTagName("td")[1];
+            const idText = (_b = idCell.textContent) === null || _b === void 0 ? void 0 : _b.trim().toLowerCase();
+            const valueText = (_c = valueCell.textContent) === null || _c === void 0 ? void 0 : _c.trim().toLowerCase();
+            if (idText === searchTerm || valueText.includes(searchTerm)) {
+                row.classList.add("bg-yellow-200");
+            }
+            else {
+                row.classList.remove("bg-yellow-200");
+            }
+        }
     }
+    else {
+        alert("No table rows found!");
+    }
+}
+// Button click event listener for search
+(_e = document.getElementById("searchButton")) === null || _e === void 0 ? void 0 : _e.addEventListener("click", () => {
+    searchEmail();
 });
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -20,16 +20,14 @@ document.getElementById("fileInput")?.addEventListener("change", () => {
 
 // Process the selected email files
 function processEmailFiles(files: FileList) {
-  const data: TableData[] = [];
-
-  // Read each file using FileReader
   // Read each file using FileReader
   for (const file of files) {
     const reader = new FileReader();
     reader.onload = (event) => {
       const text = event.target?.result as string;
       const emails = text.split(",").map((email) => email.trim());
-      displayTableData(emails);
+        displayTableData(emails, files, file.name);
+      
     };
     reader.readAsText(file);
   }
@@ -37,21 +35,54 @@ function processEmailFiles(files: FileList) {
 
 
 // Display table data
-function displayTableData(emails: any[]) {
-    const tableBody = document.getElementById('tableBody');
-    if (tableBody) {
-      tableBody.innerHTML = '';
-  
-      emails.forEach((email, index) => {
-        const newRow = document.createElement('tr');
-        newRow.innerHTML = `
-          <td class="px-4 py-2">${index + 1}</td>
-          <td class="px-4 py-2">${email}</td>
-        `;
-        tableBody.appendChild(newRow);
+function displayTableData(emails: string[], files: FileList, fileName:string) {
+  const tableBody = document.getElementById('tableBody');
+  if (tableBody) {
+    tableBody.innerHTML = '';
+
+    emails.forEach((email, index) => {
+      const newRow = document.createElement('tr');
+      newRow.innerHTML = `
+        <td class="px-4 py-2">${index + 1}</td>
+        <td class="px-4 py-2">${email}</td>
+        <td class="px-4 py-2">${fileName}</td>
+
+      `;
+      newRow.addEventListener('click', () => {
+        selectRow(newRow, index, email, emails, fileName, files);
       });
-    }
+      tableBody.appendChild(newRow);
+    });
   }
+}
+
+// Select row and enable editing
+function selectRow(row: HTMLTableRowElement, index: number, email: string, emails: string[], fileName: string | undefined, files: FileList) {
+  const selectedRow = document.querySelector('.selected');
+  if (selectedRow) {
+    selectedRow.classList.remove('selected');
+  }
+  row.classList.add('selected');
+
+  const inputValue = prompt('Edit email value:', email);
+  if (inputValue !== null) {
+    // Update the email value in the table
+    row.children[1].textContent = inputValue;
+
+    // Update the email value in the data array
+    emails[index] = inputValue;
+
+    // Update the text file
+    updateTextFile(emails, fileName, files);
+  }
+}
+
+// Update the text file with new email values
+function updateTextFile(emails: any[], fileName: string | undefined, files: FileList) {
+  const updatedText = emails.join(', ');
+}
+
+
 
 // Add to table button click event listener
 document.getElementById("addToTableButton")?.addEventListener("click", () => {
@@ -72,12 +103,38 @@ document.getElementById("addToTableButton")?.addEventListener("click", () => {
 
 // Delete last row button click event listener
 document
-  .getElementById("removeLastRowButton")
+  .getElementById("addToFile")
   ?.addEventListener("click", () => {
-    const tableRows = document
-      .getElementById("tableBody")
-      ?.getElementsByTagName("tr");
-    if (tableRows && tableRows.length > 0) {
-      tableRows[tableRows.length - 1].remove();
-    }
+    
   });
+
+
+// Search for an email by ID or value
+function searchEmail() {
+  const searchInput = document.getElementById("manualRowField") as HTMLInputElement;
+  const searchTerm = searchInput.value.trim().toLowerCase();
+  
+  const tableRows = document.getElementById("tableBody")?.getElementsByTagName("tr");
+  if (tableRows) {
+    for (const row of tableRows) {
+      const idCell = row.getElementsByTagName("td")[0];
+      const valueCell = row.getElementsByTagName("td")[1];
+      const idText = idCell.textContent?.trim().toLowerCase();
+      const valueText = valueCell.textContent?.trim().toLowerCase();
+      
+      if (idText === searchTerm || valueText!!.includes(searchTerm)) {
+        row.classList.add("bg-yellow-200");
+      } else {
+        row.classList.remove("bg-yellow-200");
+      }
+    }
+  } else {
+    alert("No table rows found!");
+  }
+}
+
+
+// Button click event listener for search
+document.getElementById("searchButton")?.addEventListener("click", () => {
+  searchEmail();
+});
